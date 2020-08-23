0de8a4da20be3e5f387ce7eb99aeb1c7a6b4c76c
julioleao <julio0023@live.com>
2020-08-23 12:34:44 -0300

    Utualizada a classe Campus do pacote models com as annotations da biblioteca Lombok, para @Getter @Setter e @ToString, para criar automaticamente os métodos get set e toString.

--- src/main/java/br/edu/utfpr/dv/siacoes/model/Campus.java
+++ src/main/java/br/edu/utfpr/dv/siacoes/model/Campus.java
@@ -2,11 +2,16 @@
 
 import java.io.Serializable;
 
+import lombok.*;
+
+@ToString(onlyExplicitlyIncluded = true)
+@Getter @Setter
 public class Campus implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
 	private int idCampus;
+	@ToString.Include
 	private String name;
 	private String address;
 	private transient byte[] logo;
@@ -14,61 +19,4 @@ public class Campus implements Serializable {
 	private String site;
 	private String initials;
 	
-	public Campus(){
-		this.setIdCampus(0);
-		this.setName("");
-		this.setAddress("");
-		this.setLogo(null);
-		this.setActive(true);
-		this.setSite("");
-		this.setInitials("");
-	}
-	
-	public int getIdCampus() {
-		return idCampus;
-	}
-	public void setIdCampus(int idCampus) {
-		this.idCampus = idCampus;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getAddress() {
-		return address;
-	}
-	public void setAddress(String address) {
-		this.address = address;
-	}
-	public byte[] getLogo() {
-		return logo;
-	}
-	public void setLogo(byte[] logo) {
-		this.logo = logo;
-	}
-	public boolean isActive() {
-		return active;
-	}
-	public void setActive(boolean active) {
-		this.active = active;
-	}
-	public String getSite(){
-		return site;
-	}
-	public void setSite(String site){
-		this.site = site;
-	}
-	public String getInitials() {
-		return initials;
-	}
-	public void setInitials(String initials) {
-		this.initials = initials;
-	}
-
-	public String toString(){
-		return this.getName();
-	}
-
 }
