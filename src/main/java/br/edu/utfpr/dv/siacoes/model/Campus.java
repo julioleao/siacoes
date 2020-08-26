9d64189bcf3183887ca5b9bcce0f288ccdf2652c
julioleao <julio0023@live.com>
2020-08-26 13:24:54 -0300

    add a annotation @NoArgsConstructor para as classes Campus e Department, a fim de criar os métodos construtores de ambas, sem argumentos.

--- src/main/java/br/edu/utfpr/dv/siacoes/model/Campus.java
+++ src/main/java/br/edu/utfpr/dv/siacoes/model/Campus.java
@@ -6,6 +6,7 @@ import lombok.*;
 
 @ToString(onlyExplicitlyIncluded = true)
 @Getter @Setter
+@NoArgsConstructor
 public class Campus implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
